c738c0ec6511cda006c606bf4de87d84aedb13ef
Govind Rai <raigovind93@gmail.com>
2017-07-25 10:57:21 -0700

    Add youtube api key to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+
 // Create a new component. This component should produce some HTML
 const App = () => <div>Hi!</div>;
 
