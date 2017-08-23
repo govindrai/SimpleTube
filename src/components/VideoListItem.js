4130833cbaebef9c8281e6ed3af4874e37f73404
Govind Rai <raigovind93@gmail.com>
2017-08-22 22:12:47 -0700

    b

--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -1,4 +1,4 @@
-import React from "React";
+import React from "react";
 
 const VideoListItem = ({ video, updateSelectedVideoHandler }) => {
   const imageURL = video.snippet.thumbnails.default.url;
