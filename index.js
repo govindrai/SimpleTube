97977b6fa8f4ac5bb1b012fdf1361fb30a83fd26
Govind Rai <raigovind93@gmail.com>
2017-08-22 23:11:29 -0700

    adj

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
