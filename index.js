133aaba69f208dd7775d41408084b2fb2abd0a0e
Govind Rai <raigovind93@gmail.com>
2017-08-22 22:52:31 -0700

    aldkjf

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", function(req, res) {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(process.env.PORT || 3000, function() {
