74171e1bba566661eac80248b446ce22524614bf
Govind Rai <raigovind93@gmail.com>
2017-08-22 22:03:38 -0700

    b

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/"));
+app.use(express.static(path.join(__dirname, "/")));
 
 app.get("*", function(req, res) {
   res.sendFile(path.resolve(__dirname, "index.html"));
