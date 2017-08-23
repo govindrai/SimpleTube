8d533faa662c692a3bbe0ca88662a13f78a6b51a
Govind Rai <raigovind93@gmail.com>
2017-08-22 21:56:48 -0700

    b

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/")));
+app.use(express.static(__dirname));
+
+app.get("*", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("SimpleTube is now listening for connections!");
