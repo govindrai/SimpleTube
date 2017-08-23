3f83c374b504cb5f689d4fd102a35972eb0a9564
Govind Rai <raigovind93@gmail.com>
2017-08-22 20:42:56 -0700

    Change static file serving settings

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const path = require("path")
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "/"));
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("SimpleTube is now listening for connections!");
